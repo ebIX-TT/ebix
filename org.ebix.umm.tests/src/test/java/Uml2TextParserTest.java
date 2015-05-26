42cfe4cf387dc9e44a38203d523ae3c56195e3b8
Mariusz Stefanowski <mariusz.stefanowski@in4mates.com>
2015-05-26 03:07:16 +0200

    Mvn support

---
+++ org.ebix.umm.tests/src/test/java/Uml2TextParserTest.java
@@ -0,0 +1,33 @@
+package test.java;
+
+
+import org.ebix.umm.uml2text.Uml2Text;
+import org.ebix.umm.uml2text.file.FileWriterStandaloneImpl;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.uml2.uml.resources.util.UMLResourcesUtil;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+
+public class Uml2TextParserTest extends BaseTestCase {
+	
+
+	@Test
+	public void shouldGenerateOdsFile(){
+	    final URI fileURI = URI.createFileURI("src/test/resources/test.uml");
+	    final FileWriterStandaloneImpl fw = new FileWriterStandaloneImpl();
+	    final ResourceSetImpl resourceSet = new ResourceSetImpl();
+	    UMLResourcesUtil.init(resourceSet);
+	    Uml2Text _uml2Text = new Uml2Text();
+	    _uml2Text.processFile(fileURI, resourceSet, fw);
+	    File generatedFile = new File("model-text/European_Energy_Market.ods");
+	    assertTrue(generatedFile.exists());
+	}
+	
+
+
+}
---
+++ org.ebix.umm.tests/src/test/java/UmmGeneratorTest.java
@@ -0,0 +1,13 @@
+package test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UmmGeneratorTest extends BaseTestCase {
+	
+	@Test
+	public void should(){
+	}
+	
+}
